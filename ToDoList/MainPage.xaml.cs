b12ca947b36b0d8d5927f1b32bdfe38c1267b6c8
Alex 'hellorush' Khorrshr <notmyrealemail@gmail.com>
2025-01-20 13:11:24 +0200

    can change striked through text jumping from one button to another

--- ToDoList/MainPage.xaml.cs
+++ ToDoList/MainPage.xaml.cs
@@ -9,24 +9,24 @@ namespace ToDoList
             InitializeComponent();
         }
 
-        void OnRadioButtonChecked(object sender, CheckedChangedEventArgs e)
+        void OnRadioButtonChk(object sender, CheckedChangedEventArgs e)
         {
-            // Handle the selection
+            System.Diagnostics.Debug.WriteLine("Event triggered");
             RadioButton radioButton = (RadioButton)sender;
 
             if (radioButton.Content is Label label)
             {
-                if (radioButton.IsChecked)
-                {
-                    // Apply strikethrough to the label's text
-                    label.TextDecorations = TextDecorations.Strikethrough;
-                    System.Diagnostics.Debug.WriteLine($"Selected option: {label.Text}");
-                }
-                else
-                {
-                    // Remove strikethrough if the radio button is deselected
-                    label.TextDecorations = TextDecorations.None;
-                }
+                System.Diagnostics.Debug.WriteLine($"Before toggle: {label.TextDecorations}");
+                label.TextDecorations = label.TextDecorations == TextDecorations.Strikethrough
+                    ? TextDecorations.None
+                    : TextDecorations.Strikethrough;
+                System.Diagnostics.Debug.WriteLine($"After toggle: {label.TextDecorations}");
+                System.Diagnostics.Debug.WriteLine($"Option toggled: {label.Text}");
+            }
+            else
+            {
+                System.Diagnostics.Debug.WriteLine("Content is not a Label");
+                System.Diagnostics.Debug.WriteLine($"Content type: {radioButton.Content.GetType().Name}");
             }
         }
     }
